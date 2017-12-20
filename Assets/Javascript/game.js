8ec6f734568d6963f5a2dbb41d85737077fed769
Ben_Cross <txbikeguy@gmail.com>
2017-12-20 11:09:44 -0600

    Still nowhere close to working, stuck on how to make a new question show up every time.

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,22 +1,28 @@
 var content = {
 	question1: {
 		question: "What is the name of Han Solo's infamous smuggling ship?",
-		answers: ["A) Slave I", "B) The Khetanna", "C) The Millennium Falcon", "D) The Death Star"]
+		answers: ["A) Slave I", "B) The Khetanna", "C) The Millennium Falcon", "D) The Death Star"],
+		correct: 2
 	},
 	question2: {
 		question: "What is the name of the bounty hunter that delivers frozen Han Solo to Jabba the Hutt?",
-		answers: ["A) Mace Windu", "B) Boba Fett", "C) Yoda", "D) Kylo Ren"]
+		answers: ["A) Mace Windu", "B) Boba Fett", "C) Yoda", "D) Kylo Ren"],
+		correct: 1
 	},
 	question3: {
 		question: "What relation is Darth Vader to Luke Skywalker?",
-		answers: ["A) Brother", "B) Husband", "C) Mentor", "D) Father"]
+		answers: ["A) Brother", "B) Husband", "C) Mentor", "D) Father"],
+		correct: 3
 	},
 	question4: {
 		question: "What is Ben Kenobi's original name?",
-		answers: ["A) Anakin", "B) Obi-Wan", "C) Qui-Gon", "D) Padme"]
+		answers: ["A) Anakin", "B) Obi-Wan", "C) Qui-Gon", "D) Padme"],
+		correct: 1
 	},
 	question5: {
-		question: ""
+		question: "What type of ship is Luke flying when he destroys the Death Star?",
+		answers: ["A) X-Wing", "B) Imperial Star Destroyer", "C) Tie Fighter", "D) T-15"],
+		correct: 0
 	},
 };
 
@@ -31,25 +37,49 @@ $(function(){
 function startScreen() {
 	$(".game").html("<h2>You will have 15 seconds to answer each question</h2><br><h2>Click the Start Button to begin</h2><br><button id='start'>START</button>");
 	$("#start").on("click", function() {
-		gameStart();
-		timer();
+		nextQuestion ();
 	});
 };
 
-function gameStart() {
-		$(".game").html("<h2>" + content.question1.question + "</h2><br><div id='answers'><p>" + content.question1.answers[0] + "</p><br><p>" + content.question1.answers[1] + "</p><br><p>" + content.question1.answers[2] + "</p><br><p>" + content.question1.answers[3] + "</p></div><div id='timer'></div>");
-		timer();
+	function q1() {
+			$(".game").html("<h2>" + content.question1.question + "</h2><br><div id='answers'><p>" + content.question1.answers[0] + "</p><br><p>" + content.question1.answers[1] + "</p><br><p id='right'>" + content.question1.answers[2] + "</p><br><p>" + content.question1.answers[3] + "</p></div><div id='timer'></div>");
+			timer();
+	};
+
+	function q2() {
+			$(".game").html("<h2>" + content.question2.question + "</h2><br><div id='answers'><p>" + content.question2.answers[0] + "</p><br><p id='right'>" + content.question2.answers[1] + "</p><br><p>" + content.question2.answers[2] + "</p><br><p>" + content.question2.answers[3] + "</p></div><div id='timer'></div>");
+			timer();
+	};
+
+	function q3() {
+			$(".game").html("<h2>" + content.question3.question + "</h2><br><div id='answers'><p>" + content.question3.answers[0] + "</p><br><p>" + content.question3.answers[1] + "</p><br><p>" + content.question3.answers[2] + "</p><br><p id='right'>" + content.question3.answers[3] + "</p></div><div id='timer'></div>");
+			timer();
+	};
+
+	function q4() {
+			$(".game").html("<h2>" + content.question4.question + "</h2><br><div id='answers'><p>" + content.question4.answers[0] + "</p><br><p id='right'>" + content.question4.answers[1] + "</p><br><p>" + content.question4.answers[2] + "</p><br><p>" + content.question4.answers[3] + "</p></div><div id='timer'></div>");
+			timer();
+	};
+
+	function q5() {
+			$(".game").html("<h2>" + content.question5.question + "</h2><br><div id='answers'><p id='right'>" + content.question5.answers[0] + "</p><br><p>" + content.question5.answers[1] + "</p><br><p>" + content.question5.answers[2] + "</p><br><p>" + content.question5.answers[3] + "</p></div><div id='timer'></div>");
+			timer();
+	};
+
+function nextQuestion() {
+	q1();
 };
 
 function timer() {
-	var timeLeft = 16;
+	var timeLeft = 15;
     var countDown = setInterval(function(){
-    timeLeft--;
     $("#timer").text(timeLeft);
-    if (timeLeft <= 0) 
+        timeLeft--;
+    if (timeLeft === 0) {
+    	timesUp();
         clearInterval(countDown);
-    	}, 1000);
-
+    	}
+    }, 1000);
     $("#timer").show();
 };
 
@@ -57,8 +87,11 @@ function timer() {
 function timesUp() {
 	$(".game").html("<h2>YOUR TIME IS UP!</h2");
 	setTimeout(function() {
-		gameStart();
+		nextQuestion();
 	}, 4000);
+
 };
 
 startScreen();
+
+
